5f8500e631e9e0721643fc093ec347a3ce9e891a
BrindalDhol23 <77315414+BrindalDhol23@users.noreply.github.com>
2021-04-23 12:06:10 -0400

    index page

---
+++ bookingindex.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Booking System</title>
+	<link rel="stylesheet" type="text/css" href="bookingindex.css">
+</head>
+
+<body>
+	<h1>Welcome to GSU Booking System!</h1>
+
+	<main class="flex-container">
+		<div class="flex-child">
+		    <h3>Book A Flight!</h3>
+		    <img src="plane.jpg" alt="plane" id="plane"><br>
+		    <p>Planning to Travel? Go to the site to check the various flight options available for you to travel to different destinations!</p>	
+		    <br>
+		   <button class="button"> <span>Go to Site</span></button>
+		</div>
+
+		<div class="flex-child">
+		    <h3>Book A Car!</h3>  
+		    <img src="car.jpg" alt="car" id="car"> <br>
+		    <p>To book a car to rent for couple or hours, go to the site to check the various options of cars and packages available!</p>
+		     <br>
+		    <button class="button"> <span>Go to Site</span></button>
+		</div>
+
+		<div class="flex-child">
+		    <h3>Book A Parking Slot!</h3> 
+		    <img src="park.jpg" alt="parking" id="parking"><br>
+		    <p>Pre-book a slot for parking of your choice by the number of hours of the time slot of your choice and pay the bill online!</p>
+		     <br>
+		    <button class="button"> <span>Go to Site</span></button>
+		</div>
+	</main>
+
+</body>
+</html>
