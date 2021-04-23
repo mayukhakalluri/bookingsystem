0eda15523045e5d468f46934fd511393db0a598e
BrindalDhol23 <77315414+BrindalDhol23@users.noreply.github.com>
2021-04-23 14:07:30 -0400

    button update

--- bookingindex.php
+++ bookingindex.php
@@ -14,7 +14,7 @@
 		    <img src="plane.jpg" alt="plane" id="plane"><br>
 		    <p>Planning to Travel? Go to the site to check the various flight options available for you to travel to different destinations!</p>	
 		    <br>
-		   <button class="button"> <span>Go to Site</span></button>
+		   <button class="button"><a href="" class="site"><span>Go to Site</span></a></button>
 		</div>
 
 		<div class="flex-child">
@@ -22,7 +22,7 @@
 		    <img src="car.jpg" alt="car" id="car"> <br>
 		    <p>To book a car to rent for couple or hours, go to the site to check the various options of cars and packages available!</p>
 		     <br>
-		    <button class="button"> <span>Go to Site</span></button>
+		    <button class="button"><a href="" class="site"><span>Go to Site</span></a></button>
 		</div>
 
 		<div class="flex-child">
@@ -30,7 +30,7 @@
 		    <img src="park.jpg" alt="parking" id="parking"><br>
 		    <p>Pre-book a slot for parking of your choice by the number of hours, for the time slot of your choice and pay the bill online!</p>
 		     <br>
-		    <button class="button"> <span>Go to Site</span></button>
+		    <button class="button"><a href="parking.php" target="_blank" class="site"><span>Go to Site</span></a></button>
 		</div>
 	</main>
 
